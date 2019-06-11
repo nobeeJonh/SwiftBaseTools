cea9bae42d64fd4db6dbb811189dca51644d0cda
wangkai <ioswangkai@163.com>
2019-06-11 15:19:24 +0800

    初始化工程

---
+++ Pods/Target Support Files/Pods-SwiftBaseTools/Pods-SwiftBaseTools-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftBaseTools : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftBaseTools
+@end
---
+++ SwiftBaseTools/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftBaseTools
+//
+//  Created by yons on 2019/6/11.
+//  Copyright © 2019年 yons. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
